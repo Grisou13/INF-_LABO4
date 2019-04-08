0d869a7a7240bb006e036d2640cfcd01121c0940
Thomas Ricci <grisou13@hotmail.fr>
2019-04-08 11:55:00 +0200

    Added exceptions

--- collection_g.h
+++ collection_g.h
@@ -1,15 +1,72 @@
+#include <iostream>
+
 #ifndef COLLECTION_G_H
 #define COLLECTION_G_H
-template<typename T, typename CONTENEUR >
+/**
+ * 
+ * Utilisable seulement par des conteneur implémentant std::allocator
+ * TODO: Utilisation avec array et tab[] ?
+ */
+template<typename T, template <typename> class CONTENEUR >
 class Collection{
    public:
-      T get(size_T indice);
-      void ajouter(T data);
-      size_t taille;
-      bool contient(T data);
+      Collection(): taille(0){};
+      ~Collection() = default;
+      T get(size_t indice);
+      void ajouter(T elem);
+      size_t taille();
+      size_t size();
+      bool contient(const T& elem);
       void vider();
-      void parcourir(T& element);
-      friend ostream& operator<<<T>(ostream& out, const Collection<T>& p);
-
+      template <typename Predicat> void parcourir(Predicat p);
+      friend std::ostream& operator<< <T>(std::ostream& out, const Collection<T>& p);
+   private:
+      CONTENEUR<T> data;
 };
+
+template<typename T, template <typename> class U >
+void Collection<T, U>::ajouter(T elem){
+   data[taille] = elem;
+   taille++;
+}
+
+template<typename T, template <typename> class U >
+T Collection<T,U>::get(size_t indice){
+   if(indice >= taille){
+      throw new IndiceNonValide("n doit etre strictement plus petit que collection.size()")
+   }
+   return *(data.begin()+indice);
+}
+
+template<typename T, template <typename> class U >
+bool Collection<T,U>::contient(const T& elem){
+   for(auto i = data.begin(); i != data.end(); ++i){
+      if(*i == elem){
+         return true;
+      }
+   }
+   return false;
+}
+
+template<typename T, template <typename> class U >
+size_t Collection<T,U>::taille(){
+   taille;
+}
+
+template<typename T, template <typename> class U >
+size_t Collection<T,U>::size(){
+   data.max_size();
+}
+
+template<typename T, template <typename> class U >
+template<typename Predicat>
+void Collection<T,U>::parcourir(Predicat p){
+   for(auto i = data.begin(); i != data.end(); ++i){
+      p(*i)
+   }
+}
+
+template <> Collection<char, std::vector>
+
+
 #endif
--- exceptions.h
+++ exceptions.h
@@ -1,8 +1,19 @@
 #include <stdexcept>
+#include <string>
 
 #ifndef EXCEPTION_H
 #define EXCEPTION_H
 
-class IndiceNonValide: public out_of_range {};
-class PrixNonValide : public invalid_argument {};
+class IndiceNonValide: public std::out_of_range {
+public :
+   explicit IndiceNonValide(const std::string& what_arg) : std::out_of_range(what_arg) {}	
+   explicit IndiceNonValide(const char* what_arg) : std::out_of_range(what_arg) {}	
+};
+
+
+class PrixNonValide : public std::invalid_argument {
+public :
+   explicit PrixNonValide(const std::string& what_arg) : std::invalid_argument(what_arg) {}	
+   explicit PrixNonValide(const char* what_arg) : std::invalid_argument(what_arg) {}	
+};
 #endif
