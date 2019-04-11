21a0633d6bf6b12fab25a3e1bc46e896e49b8bba
Fabio Marques <fabiomanuelsilvamarques@gmail.com>
2019-04-11 13:49:04 +0200

    implementation de la classe prix

--- produit.cpp
+++ produit.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
-#include <iomanip>
 #include "produit.h"
+#include <iomanip>
+#include "exceptions.h"
 
-using namespace std;
-
-std::ostream& operator<<(std::ostream& out, const Produit& p){
-   out << "("<< p.id << ", " << p.libelle << ", " << fixed << setprecision(2) << p.prix << ")";
-   return out;
+Produit::Produit(unsigned id, const std::string& libelle, double prix) : id(id), libelle(libelle) {
+   setPrix(prix);
 }
-bool operator==(const Produit& lhs, const Produit& rhs){
-   return lhs.id == rhs.id;
+
+void Produit::setPrix(double prix) {
+   if (prix < 0.05) {
+      throw PrixNonValide("le prix doit etre >= 5 cts !");
+   }
+
+   this->prix = prix;
+}
+
+std::ostream& operator<<(std::ostream& out, const Produit& p) {
+   return out  << "("
+               << p.id
+               << ", \"" << p.libelle << "\""
+               << ", " << std::fixed << std::setprecision(2) << p.prix
+               << ")";
+}
+
+bool operator==(const Produit& lhs, const Produit& rhs) {
+   return lhs.id == rhs.id && lhs.libelle == rhs.libelle && lhs.prix == rhs.prix;
 }
